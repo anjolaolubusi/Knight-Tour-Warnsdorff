3ce3dd1114d5653868c494ba097687de4526736d
Anjolaoluwa Olubusi <anjolaolubusi@gmail.com>
2020-01-09 02:09:58 -0500

    Added ability to dynamically find more knight tour's on the same chessboard

--- main.py
+++ main.py
@@ -31,18 +31,18 @@ textsurface = myfont.render('Enter Width: ', False, (0, 0, 0)) #Text to show on 
 
 def GetWidth(): #Gets the width of the chessboard
     global textsurface,string,running,chess_width
-    pygame.display.flip()
-    screen.fill(WHITE)
-    test = myfont.render(string, False, (0, 0, 0))
-    screen.blit(test,(400 + (textsurface.get_width() // 2),300 - (textsurface.get_height() // 2)) ) 
-    screen.blit(textsurface,(400  - (textsurface.get_width() // 2), 300 - (textsurface.get_height() // 2)) )
+    pygame.display.flip() #Updates the screen
+    screen.fill(WHITE) #Paints the screen white
+    test = myfont.render(string, False, (0, 0, 0)) #Text to show the input
+    screen.blit(test,(400 + (textsurface.get_width() // 2),300 - (textsurface.get_height() // 2)) ) #Shows "Enter Width: " on screen
+    screen.blit(textsurface,(400  - (textsurface.get_width() // 2), 300 - (textsurface.get_height() // 2)) ) #Shows input on screen
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if(len(event.unicode) > 0):
-                if (ord(event.unicode) < 58 and ord(event.unicode) > 47):
-                    string += pygame.key.name(event.key)
+                if (ord(event.unicode) < 58 and ord(event.unicode) > 47): #Makes sure that the input is numerical
+                    string += pygame.key.name(event.key) #Updates input string
                     pygame.display.update()
-                elif pygame.key.name(event.key) == "backspace":
+                elif pygame.key.name(event.key) == "backspace": #Removes one character from the input string
                     string = string[:len(string) - 1]
                     pygame.display.update()
                 elif pygame.key.name(event.key) == "return":
@@ -107,10 +107,10 @@ def drawRectangle(x : int, y: int):
 #Draws the Chessboard
 def DrawChessBoard():
     global ChessCursor
-    global OriginPoint
+    #global OriginPoint
     global BLOCK_WIDTH
     global BLOCK_HEIGHT
-    global screen, RED
+    global screen, RED 
     OriginPoint = np.array((BLOCK_WIDTH, BLOCK_HEIGHT))
     for i in range(chess_height):
         for j in range(chess_width):
@@ -129,7 +129,8 @@ def DrawChessBoard():
     for j in range(chess_height + 1):
         pygame.draw.line(screen, BLACK, ChessCursor, (OriginPoint[0] + (chess_width)*BLOCK_WIDTH, ChessCursor[1]))
         ChessCursor[1] += BLOCK_HEIGHT
-
+    
+    ChessCursor = np.array((BLOCK_WIDTH, BLOCK_HEIGHT))
 
 my_grid = Warnsdorff(chess_width, chess_height, start_x, start_y, BLOCK_WIDTH, BLOCK_HEIGHT) #Object that contains the knight tour and the grid
 my_grid.KnightTour() #Starts the KnightTour
@@ -139,10 +140,11 @@ ChessCursor = OriginPoint #The cursor used to draw the grid
 screen_width = int((chess_width + 2) * BLOCK_WIDTH) #The width of the new window
 screen_height = int((chess_height + 2) * BLOCK_HEIGHT) #The height of the new window
 screen = pygame.display.set_mode((screen_width, screen_height), pygame.RESIZABLE) #Resizes the window to our speficiations
+pygame.display.set_caption("PyTour: Getting Starting Position") #Sets the title of the window
 screen.fill((255, 255, 255))#Makes the background white
 DrawChessBoard() 
 
-def GetStartPos():
+def GetStartPos(): 
     global running,start_x,start_y,textsurface,string,screen
     myfont = pygame.font.SysFont('Times New Roman', int(14*math.log(math.sqrt((chess_width ** 2) + (chess_height ** 2)), 10) ))
     pygame.display.flip()
@@ -154,7 +156,7 @@ def GetStartPos():
             if(BLOCK_WIDTH <= pos[0] <= (chess_width + 1)*BLOCK_WIDTH and BLOCK_HEIGHT <= pos[1] <= (chess_height + 1)*BLOCK_HEIGHT):
                 start_x = int(pos[0]/BLOCK_WIDTH) - 1
                 start_y = int(pos[1]/BLOCK_HEIGHT) - 1
-                pygame.draw.rect(screen , WHITE, ((0,0), (screen_width // 2 + textsurface.get_width() // 2, BLOCK_HEIGHT // 2 + 10)))
+                pygame.draw.rect(screen , WHITE, ((0,0), (screen_width // 2 + textsurface.get_width() // 2, BLOCK_HEIGHT)))
                 pygame.display.flip()
                 running = False
         if event.type==QUIT:
@@ -169,20 +171,44 @@ while running:
 
 my_grid = Warnsdorff(chess_width, chess_height, start_x, start_y, BLOCK_WIDTH, BLOCK_HEIGHT) #Object that contains the knight tour and the grid
 my_grid.KnightTour() #Starts the KnightTour
-
+pygame.display.set_caption("PyTour: Animating KnightTour") #Sets the title of the window
 
 step = 0
-#Loop so that we can see and animate the knight tour
+finished = False
+def AnimateKnightTour():
+    global step, my_grid,finished,start_x, start_y,BLOCK_WIDTH,BLOCK_HEIGHT,chess_width,chess_height,screen
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN and finished == True:
+            pos = pygame.mouse.get_pos()
+            if(BLOCK_WIDTH <= pos[0] <= (chess_width + 1)*BLOCK_WIDTH and BLOCK_HEIGHT <= pos[1] <= (chess_height + 1)*BLOCK_HEIGHT):
+                start_x = int(pos[0]/BLOCK_WIDTH) - 1
+                start_y = int(pos[1]/BLOCK_HEIGHT) - 1
+            screen.fill(WHITE)
+            DrawChessBoard()
+            my_grid = Warnsdorff(chess_width, chess_height, start_x, start_y, BLOCK_WIDTH, BLOCK_HEIGHT) #Object that contains the knight tour and the grid
+            my_grid.KnightTour()
+            finished = False
+            AnimateKnightTour()
+
+    if step < len(my_grid.ListOfSteps) - 1 and finished == False:
+        pygame.draw.line(screen, RED, my_grid.ListOfSteps[step], my_grid.ListOfSteps[step + 1], 2)
+        step += 1
+
+    if step == len(my_grid.ListOfSteps) - 1:
+        step = 0
+        finished = True
+        myfont = pygame.font.SysFont('Times New Roman', int(14*math.log(math.sqrt((chess_width ** 2) + (chess_height ** 2)), 10) ))
+        pygame.display.flip()
+        textsurface = myfont.render('Click on the starting chessblock', False, (0,0,0))
+        screen.blit(textsurface, (screen_width // 2 - textsurface.get_width() // 2, BLOCK_HEIGHT // 2 - 10))
+
+
+
+
 while True:
-        for event in pygame.event.get():
-            if event.type==QUIT:
-                pygame.quit()
-                sys.exit()
-        
-        if step < len(my_grid.ListOfSteps) - 1:
-            pygame.draw.line(screen, RED, my_grid.ListOfSteps[step], my_grid.ListOfSteps[step + 1], 2)
-            step += 1
-
-        fpsClock.tick(10)
-        pygame.display.update()
-
+    fpsClock.tick(10)
+    pygame.display.update()
+    AnimateKnightTour()
